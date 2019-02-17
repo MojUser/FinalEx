1052614da6560cd72f799f8aa62539a5c9144064
Dominik.Wosinski <bl@wp.pl>
2019-02-17 15:28:13 +0100

    Zadania dodane

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.ArrayDeque;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> deque = new ArrayDeque();
+        deque.push(10);
+        deque.push(11);
+        deque.push(12);
+        deque.push(13);
+        deque.push(14);
+        System.out.println(deque);
+    }
+}
